d522f0fad577824caed044b5ebd203c8e3e3dea3
xoriant452 <81404114+xoriant452@users.noreply.github.com>
2021-04-15 12:33:11 +0530

    nam chane

--- terraform.tf
+++ terraform.tf
@@ -1,9 +1,9 @@
-resource "azurerm_resource_group" "example" {
+resource "azurerm_resource_group" "Rg1" {
   name     = "terraform-rg"
   location = "West Europe"
 }
 
-resource "azurerm_storage_account" "example" {
+resource "azurerm_storage_account" "sa1" {
   name                     = "terraform-storage"
   resource_group_name      = azurerm_resource_group.example.name
   location                 = azurerm_resource_group.example.location
